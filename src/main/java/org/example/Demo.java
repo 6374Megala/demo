9161d8762b980feb52b436488640b0c387a5cebf
megala12mr@gmail.com <megala12mr@gmail.com>
2024-01-10 13:22:10 +0530

    demo practice

---
+++ src/main/java/org/example/Demo.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import com.google.gson.Gson;
+
+public class Demo {
+    public static void main(String[] args) {
+        Gson gson=new Gson();
+        Person person=new Person("Megala",22,"Bangalore");
+
+        String jsondata=gson.toJson(person);
+        System.out.println(jsondata);
+
+
+        Person obj = gson.fromJson(jsondata, Person.class);
+
+
+        System.out.println("Object " + obj);
+    }
+
+}
